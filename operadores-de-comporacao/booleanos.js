383de40b05786fb4d9665af2499148ea63a5f0f7
BeaPc <developer.beacandido.03@gmail.com>
2023-06-01 14:22:08 -0300

    feat: Op Booleans

---
+++ operadores-de-comporacao/booleanos.js
@@ -0,0 +1,22 @@
+let numero = 5436;
+let texto = "oi";
+let vazio;
+
+let verdade = numero > 100;
+let inverdade = numero < 10;
+
+console.log("Numero", typeof verdade);
+console.log("Texto", typeof texto);
+console.log("Vazio", typeof vazio);
+console.log("Verdade", typeof verdade);
+
+console.log("-------------------");
+
+console.log("É maior?", numero > 5436);
+console.log("É maior ou igual?", numero >= 5);
+console.log("É menor ou igual?", numero <= 1000);
+
+console.log("-------------------");
+
+console.log("É igual?", texto === "Oi");
+console.log("É igual?", texto === 900);
