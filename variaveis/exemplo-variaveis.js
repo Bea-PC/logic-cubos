340e39e42198186399b3bf219b6145280bebe76a
BeaPc <developer.beacandido.03@gmail.com>
2023-06-01 14:20:16 -0300

    feat: variables

---
+++ variaveis/exemplo-variaveis.js
@@ -0,0 +1,12 @@
+const endereco = "Avenida 1";
+
+let precoDoLeite = 9.5;
+let precoDoQueijo = 30;
+
+console.log("Preço do leite:", precoDoLeite);
+console.log("Preco do queijo", precoDoQueijo);
+
+precoDoLeite = 8;
+
+console.log("Preço do leite", precoDoLeite);
+console.log("Preço do queijo", precoDoQueijo);
---
+++ variaveis/valores-variaveis.js
@@ -0,0 +1,8 @@
+let nome = "Bea";
+
+let idade = 25;
+
+idade = 18;
+
+console.log(nome);
+console.log(idade);
