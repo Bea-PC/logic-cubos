b87998d6edc0ebf7d56d699ebf51ebe28ffc9151
BeaPc <developer.beacandido.03@gmail.com>
2023-06-01 14:21:11 -0300

    feat: Op arithmetics

---
+++ operadores-aritmeticos/aritmeticos.js
@@ -0,0 +1,11 @@
+let valor = 10;
+
+console.log(valor / 9);
+console.log(valor * 9);
+console.log(valor + 9);
+console.log(valor - 9);
+console.log(valor % 9);
+
+let valor2 = "Olá";
+
+console.log(valor2 + "!");
---
+++ operadores-aritmeticos/exemplo-ar.js
@@ -0,0 +1,11 @@
+let precoProduto1 = 30.5;
+console.log("Preço do produto 1 é", precoProduto1);
+
+precoProduto1 = precoProduto1 + 1;
+console.log("Preço do produto 1 é", precoProduto1);
+
+precoProduto1 = precoProduto1 + 1;
+console.log("Preço do produto 1 é", precoProduto1);
+
+// o precoProduto1 = precoProduto1 + 1; é a mesma coisa que :
+precoProduto1 += 1.0;
