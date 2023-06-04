e8c55bbc86bebada1a6c3ac93ecd086690bed0ee
BeaPc <developer.beacandido.03@gmail.com>
2023-06-04 20:31:34 -0300

    -m

---
+++ exercicios-praticos/camadas-da-atmosfera/solucao.js
@@ -0,0 +1,11 @@
+if (altitude <= 20) {
+  return "TROPOSFERA";
+} else if (altitude <= 50) {
+  return "ESTRATOSFERA";
+} else if (altitude <= 80) {
+  return "MESOSFERA";
+} else if (altitude <= 450) {
+  return "TERMOSFERA";
+} else if (altitude <= 900) {
+  return "EXOSFERA";
+}
