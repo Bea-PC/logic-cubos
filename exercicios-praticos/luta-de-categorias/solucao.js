86ab10220e22bb1dd8059d45205b8f3cad0ed319
BeaPc <developer.beacandido.03@gmail.com>
2023-06-04 20:33:01 -0300

    feat:problem resolution

---
+++ exercicios-praticos/luta-de-categorias/solucao.js
@@ -0,0 +1,25 @@
+if (peso1 && peso2 < 55) {
+  return "Podem Lutar";
+} else {
+  return "Não podem lutar";
+}
+if (peso1 && peso2 > 55 && peso1 && peso2 < 65) {
+  return "Podem Lutar";
+} else {
+  return "Não podem lutar";
+}
+if (peso1 && peso2 > 65 && peso1 && peso2 < 75) {
+  return "Podem Lutar";
+} else {
+  return "Não podem lutar";
+}
+if (peso1 && peso2 > 75 && peso1 && peso2 < 85) {
+  return "Podem Lutar";
+} else {
+  return "Não podem lutar";
+}
+if (peso1 && peso2 >= 85) {
+  return "Podem Lutar";
+} else {
+  return "Não podem lutar";
+}
